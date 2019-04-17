6f5a42dd2d571662f97d9861d2136c51138b378c
Kyle F. Davies <kylefdavies@gmail.com>
2019-04-17 17:08:13 +0545

    Add tests for client and API
    Simple tests, but it's something.

---
+++ tests/test_api.py
@@ -0,0 +1,59 @@
+import json
+import unittest
+from base64 import b64encode
+
+from flask import url_for
+
+from app import create_app, db
+from app.models import User, Role
+
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def get_api_headers(self, username, password):
+        return {
+            "Authorization":
+                "Basic " + b64encode((username + ":" + password).encode("utf-8")).decode("utf-8"),
+            "Accept":
+                "application/json",
+            "Content-Type":
+                "application/json"
+        }
+
+    def test_no_auth(self):
+        response = self.client.get(url_for("api.get_posts"), content_type="application/json")
+        self.assertEqual(response.status_code, 401)
+
+    def test_posts(self):
+        r = Role.query.filter_by(name="User").first()
+        u = User(username="brian", password="life", active=True, role=r)
+        db.session.add(u)
+        db.session.commit()
+
+        body = "# Test post\n- One\n- Two"
+        body_html = "<h1>Test post</h1><ul><li>One</li><li>Two</li></ul>"
+        response = self.client.post("/api/v1/posts/",
+                                    headers=self.get_api_headers("brian", "life"),
+                                    data=json.dumps({"body": body}))
+        self.assertEqual(response.status_code, 201)
+        url = response.headers.get("Location")
+        self.assertIsNotNone(url)
+
+        response = self.client.get(url, headers=self.get_api_headers("brian", "life"))
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual("http://localhost" + json_response["url"], url)
+        self.assertEqual(json_response["body"], body)
+        self.assertEqual(json_response["body_html"], body_html)
---
+++ tests/test_client.py
@@ -0,0 +1,54 @@
+import unittest
+
+from app import create_app, db
+from app.models import User, Role
+
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Welcome to Kyle's junk!" in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        response = self.client.post("/auth/register",
+                                    data={
+                                        "email": "brian@example.com",
+                                        "username": "brian",
+                                        "password": "abc",
+                                        "password2": "abc"
+                                    })
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post("/auth/login",
+                                    data={
+                                        "username": "brian",
+                                        "password": "abc"
+                                    },
+                                    follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Inactive account" in response.get_data(as_text=True))
+
+        user = User.query.filter_by(username="brian").first()
+        token = user.generate_activation_token()
+        response = self.client.get(f"/auth/confirm/{token}", follow_redirects=True)
+        user.activate(token)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Account activated." in response.get_data(as_text=True))
+
+        response = self.client.get("/auth/logout", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("You have been logged out." in response.get_data(as_text=True))
