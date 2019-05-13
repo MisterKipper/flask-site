9685114f759dd1ac6f23e38b76003983ef365e32
Kyle F. Davies <kylefdavies@gmail.com>
2019-05-13 14:19:46 +0545

    Add some config to VC

---
+++ config.py
@@ -0,0 +1,55 @@
+import os
+
+import dotenv
+
+try:
+    dotenv.load_dotenv(dotenv.find_dotenv())
+except Exception as e:
+    print(f"Couldn't load .env: {e.message}")
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config():
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT"))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SUBJECT_PREFIX = "[Kyle's junk] "
+    MAIL_SENDER = os.environ.get("MAIL_SENDER")
+    ADMIN_ADDRESS = os.environ.get("ADMIN_ADDRESS")
+    POSTS_PER_PAGE = 10
+    COMMENTS_PER_PAGE = 10
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = (os.environ.get("DEV_DATABASE_URL") or
+                               "sqlite:///" + os.path.join(basedir, "dev.sqlite"))
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = (os.environ.get("TEST_DATABASE_URL") or "sqlite://")
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") or
+                               "sqlite:///" + os.path.join(basedir, "prod.sqlite"))
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig
+}
