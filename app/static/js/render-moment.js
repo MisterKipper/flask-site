fee9cb313f7b58937cda3377db4e0bfacea739cc
Kyle F. Davies <kylefdavies@gmail.com>
2019-07-01 17:44:23 +0700

    Fix moment rendering
    Needed to include my own element processing due to lack of jQuery

---
+++ app/static/js/render-moment.js
@@ -0,0 +1,23 @@
+'use strict';
+moment.locale("en");
+
+function flask_moment_render(el) {
+    el.textContent = eval('moment("' + el.getAttribute('data-timestamp') + '").' + el.getAttribute('data-format') + ';');
+    el.classList.remove('flask-moment');
+    el.style.display = '';
+}
+
+function flask_moment_render_all() {
+    var l = [];
+    document.querySelectorAll('.flask-moment').forEach(function(el) {
+        flask_moment_render(el);
+        if (el.getAttribute('data-refresh')) {
+            (function(el, interval) {
+                setInterval(function() {
+                    flask_moment_render(el);
+                }, interval);
+            })(el, el.getAttribute('data-refresh'));
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", flask_moment_render_all);
